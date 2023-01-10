f9db9319a38ce19a26cb3951c1e2479afef60b9c
Radoman-ra <rmaksim886@gmail.com>
2023-01-10 21:11:12 +0100

    init

---
+++ ArratReverse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };       
+
+            nums.L
+        }
+    }
+}
